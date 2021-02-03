8f88cb46403a875aa395ed0ef5c7f23c631da796
Andrew Lees <andrew.lees@pod-point.com>
2021-02-03 10:11:48 +0000

    Tidy up entrypoint script to use exec instead of PID wait

--- prod/start.sh
+++ prod/start.sh
@@ -6,13 +6,6 @@ ROLE=${CONTAINER_ROLE:-app}
 ENVIRONMENT=${APP_ENV:-local}
 ARTISAN="/srv/www/artisan"
 
-# PHP-FPM configuration
-export PM_MAX_CHILDREN=${PM_MAX_CHILDREN:-50}
-export PM_START_SERVERS=${PM_START_SERVERS:-10}
-export PM_MIN_SPARE_SERVERS=${PM_MIN_SPARE_SERVERS:-5}
-export PM_MAX_SPARE_SERVERS=${PM_MAX_SPARE_SERVERS:-10}
-export PM_MAX_REQUESTS=${PM_MAX_REQUESTS:-1000}
-
 ##############################
 ##   Function Definitions   ##
 ##############################
@@ -75,15 +68,7 @@ _run_database_migration() {
 }
 
 _did_receive_sigterm() {
-
-  if [ -n "$DATABASE_WORKER_PID" ]; then
-    _stop_database_worker
-  fi
-
-  if [ "$ROLE" = "scheduler" ]; then
-    _stop_scheduler
-  fi
-
+  _stop_scheduler
   exit 0
 }
 
@@ -91,35 +76,12 @@ _start_fpm() {
   exec php-fpm7 -R --nodaemonize
 }
 
-_stop_fpm () {
-  echo "Sending SIGTERM to fpm"
-  kill -SIGTERM "$FPM_PID"
-  # Wait for the fpm process to exit
-  wait "$FPM_PID"
-  echo "fpm exited"
+_start_queue_worker() {
+  exec php ${ARTISAN} queue:work --sleep=${QUEUE_SLEEP}
 }
 
-_start_database_queue_worker() {
-  php ${ARTISAN} queue:work &
-  DATABASE_WORKER_PID=$!
-}
-
-_stop_database_worker () {
-  echo "Sending SIGTERM to database worker"
-  kill -SIGTERM "$DATABASE_WORKER_PID"
-  # Wait for the Database worker process to exit
-  wait "$DATABASE_WORKER_PID"
-  echo "Database worker exited"
-}
-
-_wait_on_active_processes() {
-  if [ -n "$FPM_PID" ]; then
-    wait $FPM_PID
-  fi
-
-  if [ -n "$DATABASE_WORKER_PID" ]; then
-    wait $DATABASE_WORKER_PID
-  fi
+_wait_on_scheduler_process() {
+  wait $SCHEDULER_WAIT_PID
 }
 
 _start_scheduler() {
@@ -137,11 +99,14 @@ _stop_scheduler() {
 
   echo "SIGTERM received for Scheduler"
 
+  # We'll first send SIGTERM to the PHP process running the scheduler, this
+  # will gracefully terminate if we're in the middle of processing a job
   if kill -0 "$SCHEDULER_PID" > /dev/null 2>&1; then
       echo "Waiting for scheduled task to complete.."
       wait "$SCHEDULER_PID"
   fi
-
+  # Once the task has completed, we can terminate the (sleep 60) subshell
+  # knowing we don't have any jobs being processed.
   echo "Sending SIGTERM to Scheduler"
   kill "$SCHEDULER_WAIT_PID"
 }
@@ -153,25 +118,28 @@ _stop_scheduler() {
 _verify_artisan_is_present
 _enable_development_extensions_if_required
 _print_role_and_environment
-_register_trap
 _validate_role
 
-if [ "$ROLE" = "scheduler" ]; then
+# Launch container for specific role.
+case $ROLE in
+"scheduler")
+  _register_trap
   _start_scheduler
-fi
-
-_prepare_app_for_production
-
-if [ "$ROLE" = "app" ]; then
+  _wait_on_scheduler_process
+  ;;
+"app")
+  _prepare_app_for_production
   _run_database_migration
-fi
+  _start_fpm
+  ;;
+"queue")
+  _start_queue_worker
+  ;;
+*)
+  echo "Invalid container role" && exit 1
+  ;;
+esac
 
-if [ "$ROLE" = "queue" ]; then
-  _start_database_queue_worker
-fi
-
-_start_fpm
-_wait_on_active_processes
 
 
 
